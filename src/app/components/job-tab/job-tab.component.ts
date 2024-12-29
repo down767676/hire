9ec18cf1b233bf2d5467841f981ab5e2b3a7f341
down767676 <deepak@cloudhr.us>
2024-12-29 03:36:23 -0800

    prod version 6.3

--- src/app/components/job-tab/job-tab.component.ts
+++ src/app/components/job-tab/job-tab.component.ts
@@ -66,7 +66,7 @@ export class JobTabComponent extends BaseTabComponent {
     { "code": "Texted.90", "value": "Texted in last 90 days", "map":"job_text_history" },
     { "code": "Texted.365", "value": "Texted in last 365 days", "map":"job_text_history" },
     { "code": "TextPipeline.0", "value": "Text Pipeline", "map":"job_text_pipeline" },
-
+    { "code": "TextPipeline.1", "value": "Nearby Job Text Pipeline", "map":"job_text_pipeline" },
   ]
 
   someMethod(): void {
